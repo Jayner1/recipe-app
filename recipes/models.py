e674c199fe105c27c067405a26228419bcf434f7
Jayner1 <106836096+Jayner1@users.noreply.github.com>
2023-05-17 10:14:51 -0700

    2.5

--- recipes/models.py
+++ recipes/models.py
@@ -1,12 +1,5 @@
 from django.db import models
 
-class Type(models.Model):
-    name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.name
-
-
 class Recipe(models.Model):
     TYPE_CHOICES = (
         ('Breakfast', 'Breakfasts'),
@@ -21,7 +14,7 @@ class Recipe(models.Model):
     difficulty = models.CharField(max_length=50)
     pic = models.ImageField(upload_to='media/', null=True, blank=True)
     recipe_type = models.CharField(max_length=10, choices=TYPE_CHOICES, default='Breakfast')
-    type = models.ForeignKey(Type, on_delete=models.CASCADE, null=True, blank=True)
+
 
     def __str__(self):
         return self.recipe_name
