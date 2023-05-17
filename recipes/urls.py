c126b6ab8366c85203974266680e870f88dc2f69
Jayner1 <106836096+Jayner1@users.noreply.github.com>
2023-05-17 15:27:08 -0700

    2.6

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from .views import home
 from .views import RecipeListView
+from . import views
 
 app_name = 'recipes'
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', views.login_view, name='login'),  # Set login view as the default page
+    path('home/', home, name='home'),
     path('list/', RecipeListView.as_view(), name='list'),
+    path('login/', views.login_view, name='login'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Recipe
 from django.views.generic import ListView
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -18,3 +20,18 @@ class RecipeListView(ListView):
         if recipe_type:
             queryset = queryset.filter(recipe_type=recipe_type)
         return queryset
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('recipes:home')  # Replace 'recipes:home' with the actual URL name of the home page
+        else:
+            error_message = 'Invalid username or password'
+            return render(request, 'login.html', {'error_message': error_message})
+    else:
+        return render(request, 'login.html')
