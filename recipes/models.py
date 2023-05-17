be72a9bfb3bc3b1977b51575415aa532a9689b5e
Jayner1 <106836096+Jayner1@users.noreply.github.com>
2023-05-17 09:55:08 -0700

    2.5

--- recipes/models.py
+++ recipes/models.py
@@ -1,14 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
 
 class Recipe(models.Model):
+    TYPE_CHOICES = (
+        ('Breakfast', 'Breakfasts'),
+        ('Lunch', 'Lunches'),
+        ('Dinner', 'Dinners'),
+    )
+
     id = models.AutoField(primary_key=True)
     recipe_name = models.CharField(max_length=255)
     ingredients = models.TextField()
     cooking_time = models.CharField(max_length=10)
     difficulty = models.CharField(max_length=50)
     pic = models.ImageField(upload_to='media/', null=True, blank=True)
+    recipe_type = models.CharField(max_length=10, choices=TYPE_CHOICES, default='Breakfast')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.recipe_name
+
