8fc180c6854b65485c82fa5738a3ecbef738c157
Jayner1 <106836096+Jayner1@users.noreply.github.com>
2023-05-24 17:35:55 -0700

    2.8

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['immense-ocean-10427.herokuapp.com','macrofriendlyrecipes-jasontyale.b4a.run', 'https://immense-ocean-10427.herokuapp.com']
+ALLOWED_HOSTS = ['immense-ocean-10427.herokuapp.com','127.0.0.1', 'https://immense-ocean-10427.herokuapp.com']
 
 
 
